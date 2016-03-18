798c0707590f70cf92e23a08b6e88f2b55bc0f96
RoneyGoncalves <ro.roney@hotmail.com>
2016-03-18 12:51:00 -0300

    First commit

---
+++ src/defalt/GradeBook.java
@@ -0,0 +1,10 @@
+package defalt;
+
+public class GradeBook {
+	//Exibe a mensagem de boas vindas
+	public void displayMessage()
+	{
+		System.out.println("Welcome to the Grade Book!");
+	} //Fim do método
+
+}//Fim da classe GradeBook
---
+++ src/defalt/MainGradeBookTest.java
@@ -0,0 +1,16 @@
+package defalt;
+
+public class MainGradeBookTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Cria um objeto para chamar os métodos da classe GradeBook
+		GradeBook myGradeBook = new GradeBook();
+		
+		//Chama o método displayMessage da classe GradeBook
+		myGradeBook.displayMessage();
+
+	}//Fim do metodo main
+
+}//Fim da classe MainGradeBookTest

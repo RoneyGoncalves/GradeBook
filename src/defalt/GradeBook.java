022e3276645db0fa96f9920eeed3ec81918cb5fd
RoneyGoncalves <ro.roney@hotmail.com>
2016-03-18 13:21:20 -0300

    GradeBook version 5.0

--- src/defalt/GradeBook.java
+++ src/defalt/GradeBook.java
@@ -5,19 +5,21 @@ package defalt;
 public class GradeBook {
 	
 	private String courseName; //Variável de instância courseName
+	private String courseInstructorName; //Variável de instancia para armazenar o nome do instrutor do curso
 	
 	//Construtor para inicializar courseName com um argumento String
 	/*Colocando um construtor podemos criar vários objetos, na classe main,
 	 * com inicializações diferentes.*/
-	public GradeBook(String Name)
+	public GradeBook(String NameOfCourse, String NameOfCourseInstructor)
 	{
-		courseName = Name; //Inicializa courseName
+		courseName = NameOfCourse; //Inicializa courseName
+		courseInstructorName = NameOfCourseInstructor; //Inicializa courseInstructorname
 	}
 	
 	//Metodo que Configura o nome do curso
-	public void setCourseName(String Name) //Aqui Name é passado como parâmetro
+	public void setCourseName(String NameOfCourse) //Aqui Name é passado como parâmetro
 	{
-		courseName = Name; //Aqui Name é passado como argumento
+		courseName = NameOfCourse; //Aqui Name é passado como argumento
 	}
 	
 	//Metodo que recupera o nome do curso
@@ -26,11 +28,23 @@ public class GradeBook {
 		return courseName;
 	}
 	
+	public void setCourseInstructorName(String NameOfCourseInstructorName)
+	{
+		courseInstructorName = NameOfCourseInstructorName;
+	}
+	
+	public String getCourseInstructorName()
+	{
+		return courseInstructorName;
+	}
+	
 	//Método para apresentar a mensagem
 	public void displayMessage()
 	{
 		//Chama getCourseName para obter o nome do curso
-		System.out.printf("Welcome to the Grade Book for: \n%s!\n",getCourseName());
+		System.out.printf("Welcome to the Grade Book for: %s!\n",getCourseName());
+		//Apresenta o nome do instrutor do curso
+		System.out.printf("This course is presented by: %s!",getCourseInstructorName());
 	}
 
 }//Fim da classe GradeBook
--- src/defalt/MainGradeBookTest.java
+++ src/defalt/MainGradeBookTest.java
@@ -1,21 +1,21 @@
 package defalt;
 
-import java.util.Scanner;
-
 public class MainGradeBookTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		/*Cria dois objetos com argumentos de inicialização diferentes 
-		 para a variável de instância courseName da classe GradeBook*/
-		GradeBook gradeBook1 = new GradeBook("Fuck of Java programing 1");
-		GradeBook gradeBook2 = new GradeBook("Java fucking you, second chapter!");
+		 para a variável de instância courseName e courseInstructorName da classe GradeBook*/
+		GradeBook gradeBook1 = new GradeBook("Fuck of Java programing 1","Baracho");
+		GradeBook gradeBook2 = new GradeBook("Java fucking you, second chapter!","Barachinho");
 		
-		/*Exibe o valor inicial da variável courseName dentro de cada objeto criado acima
-		 chamando o método getCourseName*/
-		System.out.printf("Object gradeBook1 couseName is: \n%s\n!",gradeBook1.getCourseName());
-		System.out.printf("Object gradeBook1 courseName is: \n%s\n",gradeBook2.getCourseName());
+		/*Exibe o valor inicial das variáveis de instância dentro de cada objeto*/
+		System.out.printf("Object gradeBook1 couseName is: %s!\n",gradeBook1.getCourseName());
+		System.out.printf("and couseInstructorName is: %s!\n",gradeBook1.getCourseInstructorName());
+		System.out.println(); //Pula uma linha
+		System.out.printf("Object gradeBook1 courseName is: %s!\n",gradeBook2.getCourseName());
+		System.out.printf("and couseInstructorName is: %s!\n",gradeBook2.getCourseInstructorName());
 		
 	}//Fim do metodo main
 
